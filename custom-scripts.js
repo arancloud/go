cc3d9f588b6147d2d05fb62f21a05f492bc78a79
Aekthawat <aranseemusic@gmail.com>
2018-05-22 21:42:36 +0700

    Add files via upload

---
+++ custom-scripts.js
@@ -0,0 +1,23 @@
+function btcFunc() {
+    var copyText = document.getElementById("BTC");
+    copyText.select();
+    document.execCommand("Copy");
+}
+
+function ltcFunc() {
+    var copyText = document.getElementById("LTC");
+    copyText.select();
+    document.execCommand("Copy");
+}
+
+function bchFunc() {
+    var copyText = document.getElementById("BCH");
+    copyText.select();
+    document.execCommand("Copy");
+}
+
+function dogFunc() {
+    var copyText = document.getElementById("DOG");
+    copyText.select();
+    document.execCommand("Copy");
+}
